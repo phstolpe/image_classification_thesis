3913337bc3c628879ba6f7518b350e3f23a6d0e8
phstolpe <123520279+phstolpe@users.noreply.github.com>
2025-09-09 10:30:26 +0200

    del pycache from tracking and update how data is outputted

--- experiment_runner.py
+++ experiment_runner.py
@@ -30,28 +30,31 @@ results = {}
 #
 #     ]}
 
-with open('test.txt', 'a') as f:
-    f.write('Dataset 1 \n\t Splits \n\t\t 80/20:')
 
 
 
+for i in range(len(splits)):
+    split_info = f'\n\t Splits \n\t\t {splits[i]}:'
 
-for i in range(3):
-    res = exp.run(i, splits[0])
-    accs.append(res["accuracy"])
     with open('test.txt', 'a') as f:
-        output = f'\n\t\t\t Iteration: {i}, Accuracy: {res["accuracy"]}'
-        f.write(output)    
+        f.write(split_info)
 
-    #add average on last iteration
-    if i == 2:
-        avg = accs[0] + accs[1] + accs[2]
-        print(avg)
-        avg = avg/3
-        print(avg)
-        with open('test.txt', 'a' ) as f:
-            output = f'\n\t\t Average: {avg}'
-            f.write(output)
+    for j in range(3):
+        res = exp.run(j, splits[0])
+        accs.append(res["accuracy"])
+        with open('test.txt', 'a') as f:
+            output = f'\n\t\t\t Iteration: {j}, Accuracy: {res["accuracy"]}'
+            f.write(output)    
+
+        #add average on last iteration
+        if j == 2:
+            avg = accs[0] + accs[1] + accs[2]
+            print(avg)
+            avg = avg/3
+            print(avg)
+            with open('test.txt', 'a' ) as f:
+                output = f'\n\t\t Average: {avg}'
+                f.write(output)
 
 
 
