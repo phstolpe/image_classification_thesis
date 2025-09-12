6989b388faa923ac962433e3054a19144f3c6cf1
phstolpe <123520279+phstolpe@users.noreply.github.com>
2025-09-12 14:23:53 +0200

    add table function to grapher script and resulting tables from experiment data

--- grapher.py
+++ grapher.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 
 
 
@@ -18,14 +19,8 @@ def extract_and_average(filename):
 
 def create_graph(result_file1, result_file2):
     p, a = extract_and_average(result_file1)
-    print('dataset 1')
-    for i in range(len(p)):
-        print(f'percentage: {p[i]} and average: {a[i]}')
 
     p2, a2 = extract_and_average(result_file2)
-    print('dataset 2')
-    for i in range(len(p2)):
-        print(f'percentage: {p2[i]} and average: {a2[i]}')
 
 
     #y axis will be accuracy and x axis will be percentage
@@ -37,12 +32,46 @@ def create_graph(result_file1, result_file2):
     plt.grid(True)
     plt.legend()
     plt.savefig('result_graph.pdf')
-    plt.show()
 
+def create_table(filename: str, title: str):
+    data = []
+    with open(filename, 'r')as f:
+        for line in f:
+            if(line.startswith('Percentage')):
+                percentage_used_for_testing = float(line.split(':')[1].strip())
+                data.append(percentage_used_for_testing)
+            elif(line.strip().startswith('Iteration')):
+                accuracy = float(line.split('Accuracy:')[1].strip())
+                data.append(accuracy)
+            elif(line.strip().startswith('Average')):
+                average = float(line.split(':')[1].strip())
+                data.append(average)
+
+    data = np.array_split(data, 4)
+    df = pd.DataFrame(data, columns = ['split', 'run_1', 'run_2', 'run_3', 'average'])
+    df['std'] = df[['run_1', 'run_2', 'run_3']].std(axis=1)
+    df = df.round(3)
+
+    fig, ax = plt.subplots(figsize=(10,2))
+
+    fig.patch.set_visible(False)
+    ax.axis('off')
+    ax.axis('tight')
+    ax.set_title(title)
+    table = ax.table(cellText=df.values, colLabels=df.columns, loc='center', cellLoc='center')
+    # fig.tight_layout()
+    table.auto_set_font_size(False)
+    table.set_fontsize(12)
+    table.scale(1,1.5)
+    
+    plt.savefig(title + '_fig.pdf')
 
 #results files
 file1 = 'result1.txt'
 file2 = 'result2.txt'
 
+
 create_graph(file1, file2)
+create_table(file1, 'Dataset 1')
+create_table(file2, 'Dataset 2')
 
